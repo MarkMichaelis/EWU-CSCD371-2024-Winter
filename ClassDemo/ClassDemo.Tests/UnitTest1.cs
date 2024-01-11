f82e8f19e741c95b941a57618318e9f7ab509b0c
Mark Michaelis <mark@IntelliTect.com>
2024-01-11 15:56:48 -0800

    feat: Add first test

--- ClassDemo/ClassDemo.Tests/UnitTest1.cs
+++ ClassDemo/ClassDemo.Tests/UnitTest1.cs
@@ -5,5 +5,7 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        // Ctrl R + T to run test
+        Program.Main(null);
     }
 }
--- ClassDemo/ClassDemo/Program.cs
+++ ClassDemo/ClassDemo/Program.cs
@@ -1,9 +1,14 @@
 ﻿namespace ClassDemo;
 
-internal class Program
+// Public is an "Access Modifier" Default is internal
+public class Program
 {
-    static void Main(string[] args)
+    // Public is an "Access Modifier". Default is private
+    public static void Main(string[] args)
     {
+        // F12 to Go-To Method, Ctrl - to "go back"
         Console.WriteLine("Hello, World!");
+        "1".ToString();
+        Console.WriteLine(1.GetType());
     }
 }
